01202714c1b370b1770114905749c55226a63988
nizamra <nizamra_95@hotmail.com>
2021-10-08 18:38:21 +0300

    music in full

--- headFirst/music/Risky.java
+++ headFirst/music/Risky.java
@@ -1,17 +1,16 @@
-public class Risky {
-    public void takeRisk() throws BadException {
-        if (abandonAllHope) {
-            throw new BadException();
-        }
-    }
-
-    public void crossFingers() {
-        Risky anObject = new Risky();
-        try {
-            anObject.takeRisk();
-        } catch (BadException ex) {
-            System.out.println("Aaargh!");
-            ex.printStackTrace();
-        }
-        }
-}
+// public class Risky {
+//     public void takeRisk() throws BadException {
+//         if (abandonAllHope) {
+//             throw new BadException();
+//         }
+//     }
+//     public void crossFingers() {
+//         Risky anObject = new Risky();
+//         try {
+//             anObject.takeRisk();
+//         } catch (BadException ex) {
+//             System.out.println("Aaargh!");
+//             ex.printStackTrace();
+//         }
+//         }
+// }
